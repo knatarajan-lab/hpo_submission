f9d1a3f0f4853c0c9f980c82ba43937fa485dfac
Zoey Jiang <xj2193@cumc.columbia.edu>
2023-03-06 17:09:48 -0500

    Update README.md file

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
-### Run submission pipeline
+## Run submission pipeline
+
+### Get Started
+
+#### Install dependencies
+```
+pip install -r requirements.txt
+```
+
+#### Create folder for holding final submission files
+```
+mkdir submission_folder
+```
+#### Fill in the config file with your database credentials in `a_dm_aou.ini` file
+```
+[DEFAULT]
+server =
+database =
+schema =
+user =
+password =
+```
+
+#### Run pipeline
+```
+sh run_pipeline.sh
+```
+
+### aou-ehr-file-check
+
+This is a submodule included to check the format of submission files. It is initialized and updated 
+from [aou-ehr-file-check](https://github.com/all-of-us/aou-ehr-file-check) repo.
+
+In order to initialize and update the submodule, run the following commands:
 
 #### Initialize submodule
 ```
@@ -10,3 +43,8 @@ git submodule update --init
 git submodule sync
 git submodule update --remote --merge
 ```
+
+### Submit to the All of Us Research Program
+You will find the submission files in folder `submission_folder`.
+
+Please follow the steps described in [Transferring Data to the DRC](https://aou-ehr-ops.zendesk.com/hc/en-us/articles/1500012461721-Transferring-Data-to-the-DRC) to submit your data to the All of Us Research Program.
--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,32 +1,35 @@
 #!/bin/sh
-cd /project/hpo/hpo_submission/
+# The commented out lines are for extracting the difference between the old and new patient_status files. Then you can
+# manually update the files to HealthPro.
 
-rm patient_status/patient_status_union.csv
-rm patient_status/patient_status_diff.csv
-rm -r patient_status_old
-cp -r patient_status/ patient_status_old
-cat patient_status_old/*csv > patient_status_old/patient_status_union_old.csv
+#cd /hpo_submission/
+
+#rm patient_status/patient_status_union.csv
+#rm patient_status/patient_status_diff.csv
+#rm -r patient_status_old
+#cp -r patient_status/ patient_status_old
+#cat patient_status_old/*csv > patient_status_old/patient_status_union_old.csv
 
 . ./venv/bin/activate && python3 ./submission_pipeline.py -db_ini a_dm_aou.ini -sql omop_table_sql/ -r aou-ehr-file-check/resources/omop/ -o HPO_submission/
 
-cat patient_status/*csv > patient_status/patient_status_union.csv
-`bash -c "comm -2 -3 <(sort patient_status/patient_status_union.csv) <(sort patient_status_old/patient_status_union_old.csv) >patient_status/patient_status_diff.csv" ` 
+#cat patient_status/*csv > patient_status/patient_status_union.csv
+#`bash -c "comm -2 -3 <(sort patient_status/patient_status_union.csv) <(sort patient_status_old/patient_status_union_old.csv) >patient_status/patient_status_diff.csv" `
 
-cd /project/hpo/submission_folder
+cd /submission_folder
 mkdir "$(date +'%Y-%m-%d')-v1"
 
-cd /project/hpo/hpo_submission/HPO_submission
+cd /HPO_submission
 
 #copy csv files into submission folder
 for f in *.csv
 do
-  cp -v "$f" /project/hpo/submission_folder/"$(date +'%Y-%m-%d')-v1"/${f%.csv}.csv
+  cp -v "$f" /submission_folder/"$(date +'%Y-%m-%d')-v1"/${f%.csv}.csv
 
 done
 
 #copy jsonl file into submission folder
 for j in *.jsonl
 do
-  cp -v "$j" /project/hpo/submission_folder/"$(date +'%Y-%m-%d')-v1"/${j%.jsonl}.jsonl
+  cp -v "$j" /submission_folder/"$(date +'%Y-%m-%d')-v1"/${j%.jsonl}.jsonl
 
 done
